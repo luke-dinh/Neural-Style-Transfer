55d5f20d326b9d9035635e4e31084664ce09837f
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-30 15:43:44 +0700

    Update

--- demo..py
+++ demo..py
@@ -6,3 +6,8 @@ MEAN_SUBTRACTIONS = [103.939, 116.779, 123.680]
 parser = argparse.ArgumentParser()
 parser.add_argument("--image_path", type=str, dest="image_path", required=True, help="Path to load images")
 parser.add_argument("--model_path", type=str, dest="model_path", required=True, help="Path to load models")
+opt = parser.parse_args()
+
+image_path = opt.image_path
+model_path = opt.model_path
+
